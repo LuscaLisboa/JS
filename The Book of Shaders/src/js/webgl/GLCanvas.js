75bc3360409bdb3c3b38e779212c1af2337a4b1b
Lucas G. Lisboa <lglisboa1503@gmail.com>
2025-02-19 20:13:14 -0300

    ./src/js/wegl/GLCanvas.js - multiple canvas

---
+++ The Book of Shaders/src/js/webgl/GLCanvas.js
@@ -0,0 +1,58 @@
+import { WebGLContext } from './WebGLContext';
+import { ShaderProgram } from './ShaderProgram';
+
+export class Canvas {
+    constructor(canvasId, vertexShaderSource, fragmentShaderSource) {
+            this.canvas = document.querySelector(canvasId);
+            this.context = new WebGLContext(this.canvas);
+            this.gl = this.context.gl;
+            
+            this.shaderProgram = new ShaderProgram(
+                this.gl,
+                vertexShaderSource,
+                fragmentShaderSource
+            );
+            
+            this.init();
+            this.animate();
+        }
+    
+        init() {
+            const positions = new Float32Array([
+                -1.0, -1.0,
+                 1.0, -1.0,
+                -1.0,  1.0,
+                 1.0,  1.0,
+            ]);
+    
+            const positionBuffer = this.gl.createBuffer();
+            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
+            this.gl.bufferData(this.gl.ARRAY_BUFFER, positions, this.gl.STATIC_DRAW);
+    
+            const positionAttributeLocation = this.shaderProgram.getAttribLocation('a_position');
+            this.gl.enableVertexAttribArray(positionAttributeLocation);
+            this.gl.vertexAttribPointer(
+                positionAttributeLocation,
+                2,              // 2 componentes per iteration
+                this.gl.FLOAT,  // datatype = 32bit float
+                false,          // !normalize data
+                0,              // 0 = move to next par per iteration
+                0               // starts on buffer
+            );
+        }
+    
+        animate() {
+            const render = (time) => {
+                time *= 0.001;  // convert to seconds
+    
+                this.shaderProgram.use();
+                const timeLocation = this.shaderProgram.getUniformLocation('u_time');
+                this.gl.uniform1f(timeLocation, time);
+    
+                this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4);
+                requestAnimationFrame(render);
+            };
+            
+            requestAnimationFrame(render);
+        }
+}
